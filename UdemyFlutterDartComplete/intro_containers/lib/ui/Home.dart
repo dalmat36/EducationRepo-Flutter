414fd5f203db51fac141090927f030d409665e01
Matt Dalesio <matthew.dalesio@outlook.com>
2019-03-02 16:47:15 -0500

    ink well widget

--- UdemyFlutterDartComplete/intro_containers/lib/ui/Home.dart
+++ UdemyFlutterDartComplete/intro_containers/lib/ui/Home.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-
-  void _onPressed(){
+  void _onPressed() {
     debugPrint("Search Pressed");
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-          appBar: new AppBar(
-          backgroundColor: Colors.greenAccent,
-            title: new Text("Fancy Day"),
-            actions: <Widget>[
-              new IconButton(icon: new Icon(Icons.send), onPressed: () => debugPrint("Send Pressed")),
-              new IconButton(icon: new Icon(Icons.search), onPressed: _onPressed)
-            ],
-          ),
-        );
+      appBar: new AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: new Text("Fancy Day"),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.send),
+              onPressed: () => debugPrint("Send Pressed")),
+          new IconButton(icon: new Icon(Icons.search), onPressed: _onPressed)
+        ],
+      ),
+      backgroundColor: Colors.grey.shade200,
+      body: new Container(
+        alignment: Alignment.center,
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Bonni",
+                style: new TextStyle(
+                    fontSize: 14.5,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.deepOrange)),
+            new InkWell(
+              child: new Text("Button!"),
+              onTap: () => debugPrint("Button Pressed"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
