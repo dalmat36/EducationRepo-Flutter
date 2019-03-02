783194e16eccf559a1cf3e96ac7865dee00c3343
Matt Dalesio <matthew.dalesio@outlook.com>
2019-03-02 15:29:01 -0500

    Completed scaffold and appbar

--- UdemyFlutterDartComplete/intro_containers/lib/main.dart
+++ UdemyFlutterDartComplete/intro_containers/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Layouts',
+      title: 'Scaffold',
       home: new Home(),
     );
   }
--- UdemyFlutterDartComplete/intro_containers/lib/ui/Home.dart
+++ UdemyFlutterDartComplete/intro_containers/lib/ui/Home.dart
@@ -1,54 +1,22 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+
+  void _onPressed(){
+    debugPrint("Search Pressed");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Container(
-        color: Colors.greenAccent,
-        alignment: Alignment.center,
-        child: new Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text("Item 1",
-                  textDirection: TextDirection.ltr,
-                  style: new TextStyle(
-                      fontSize: 12.9)),
-              new Text("Item 2",
-                  textDirection: TextDirection.ltr,
-                  style: new TextStyle(
-                      fontSize: 12.9)),
-              const Expanded(
-                child: const Text("Item 3")
-              )
-              ]
-        )
-        /*child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text("First Item",
-                textDirection: TextDirection.ltr,
-                style: new TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 18.3)),
-            new Text("Second Item",
-                textDirection: TextDirection.ltr,
-                style: new TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w900,
-                    fontSize: 18.3)),
-            new Container(
-                color: Colors.deepOrange.shade50,
-                alignment: Alignment.bottomLeft,
-                child: new Text("Third Item",
-                    textDirection: TextDirection.ltr,
-                    style: new TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w900,
-                        fontSize: 18.3))
-            )
-          ],
-        )*/
+    return new Scaffold(
+          appBar: new AppBar(
+          backgroundColor: Colors.greenAccent,
+            title: new Text("Fancy Day"),
+            actions: <Widget>[
+              new IconButton(icon: new Icon(Icons.send), onPressed: () => debugPrint("Send Pressed")),
+              new IconButton(icon: new Icon(Icons.search), onPressed: _onPressed)
+            ],
+          ),
         );
   }
 }
