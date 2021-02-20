b42cfccd611b7063289e776d397338f56856353d
Matt Dalesio <matthew.dalesio@outlook.com>
2021-02-20 14:29:53 -0500

    remove old project

--- coronavirus_app/coronavirus_app/android/app/src/main/kotlin/com/example/coronavirusapp/MainActivity.kt
+++
@@ -1,9 +0,0 @@
-package com.example.coronavirusapp
-
-import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
-
-class MainActivity: FlutterActivity() {
-}
